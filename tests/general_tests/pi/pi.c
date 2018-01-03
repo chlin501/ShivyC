6079ccbcee7912815b862607f19321542fc24bef
Shivam Sarodia <ssarodia@gmail.com>
2018-01-03 18:51:00 -0500

    return 0 manually at end

--- tests/general_tests/pi/pi.c
+++ tests/general_tests/pi/pi.c
@@ -11,11 +11,11 @@
 #include <stdio.h>
 
 int main(){
-  return 0;
-
   int a = 10000, b = 0, c=2800, d = 0, e = 0, f[2801], g = 0;
   for(; b-c;) f[b++]=a/5;
   for(; d=0, g=c*2; c-=14, printf("%.4d",e+d/a), e = d%a)
     for(b=c; d+=f[b]*a, f[b]=d%--g, d/=g-- ,--b; d*=b);
   printf("\n");
+
+  return 0;
 }
